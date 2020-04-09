ce7bafc36e69dac72b90f4c5a5f674c27d2ffdb3
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2020-04-09 17:12:41 -0400

    Updated Data
    Made a function to put the data in the format of a table

--- Data-Aggregator/index.js
+++ Data-Aggregator/index.js
@@ -2,3 +2,59 @@ var http = require("http");
 var request = require("request");
 
 var request_body = undefined;
+
+function createHtmlStringFromJson(retrievedData)
+{
+    //This creates the beginning of the html string
+    var htmlString = '<html><head><title>Data Aggregator</title></head><body><table>';
+
+    //This adds a row to the html string
+    htmlString += '<tr>';
+
+    //This loops through the data and creates a col for each category that is not a json object
+    for(var attribute in retrievedData[0])
+    {
+        if(typeof retrievedData[0][attribute] !=='object' )
+        {
+            htmlString += '<td>' + attribute + '</td>'
+        }
+
+    }
+    htmlString += '</tr>';
+
+    retrievedData.forEach(function(object){
+        htmlString += '<tr>';
+        for(var attribute in object)
+        {
+            if(typeof object[attribute] != 'object')
+            {
+                htmlString += '<td>' + object[attribute] + '</td>'
+            }
+        }
+        htmlString += '</tr>';
+    });
+    htmlString += '</table></body></html>';
+
+    return htmlString;
+}
+
+
+
+request('https://www.bnefoodtrucks.com.au/api/1/trucks', function(err, request_res, body)
+    {
+        request_body = body;
+    }
+);
+
+http.createServer(function(req, res)
+{
+    if(request_body)
+    {
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.end(createHtmlStringFromJson(JSON.parse(request_body)));
+    }
+    else {
+        res.writeHead(200, {'Content-Type' : 'text/plain'});
+        res.end('Nothing retrieved yet');
+    }
+}).listen(8080);
